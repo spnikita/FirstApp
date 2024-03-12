e691f25782c413f79759fc2b2e7ba69dba695743
nikit <nikit@LAPTOP-8R6RPS7E>
2024-03-12 18:02:54 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Nikita";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
